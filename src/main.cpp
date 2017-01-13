b78f38fc92e8938d10578293c90997ee7f5a31cc
Jack <klyshko.jack@gmail.com>
2017-01-13 12:12:18 -0500

    Need to solve why second pole ois skinny

--- src/main.cpp
+++ src/main.cpp
@@ -38,9 +38,9 @@ using namespace std;
 
 #define VISCOSITY		600 //xz which units and why?
 
-#define ELLIPSE_A		2000
-#define ELLIPSE_B		1000
-#define ELLIPSE_C		1000
+#define ELLIPSE_A		8000
+#define ELLIPSE_B		5000
+#define ELLIPSE_C		5000
 
 #define KIN_RADIUS			300
 #define KIN_HEIGHT			500
@@ -72,7 +72,7 @@ typedef struct{
 
 //////////////////////////////////////////////////////////////////////////////////////
 
-MT *mt1, *mt2;
+MT* pol[2];
 MDSystem mds;
 
 //////////////////////////////////////////////////////////////////////////////////////
@@ -103,17 +103,16 @@ int main(){
 	mds.N = 2 * MT_NUM*MAX_MT_LENGTH + kinetochore.size() - 1;
 	mds.r = (float3*)calloc(mds.N, sizeof(float3));
 	mds.f = (float3*)calloc(mds.N, sizeof(float3));
-	mt1 = (MT*)calloc(MT_NUM, sizeof(MT));
-	mt2 = (MT*)calloc(MT_NUM, sizeof(MT));
-	int m, i;
-	for(m = 0; m < MT_NUM; m++){
-		mt1[m].r = &mds.r[MAX_MT_LENGTH*m];
-		mt1[m].n = 2;
-		mt1[m].state = MT_STATE_POL;
-
-		mt2[m].r = &mds.r[MT_NUM*MAX_MT_LENGTH + MAX_MT_LENGTH*m];
-		mt2[m].n = 2;
-		mt2[m].state = MT_STATE_POL;		
+	pol[0] = (MT*)calloc(MT_NUM, sizeof(MT));
+	pol[1] = (MT*)calloc(MT_NUM, sizeof(MT));
+	int m, i, j;
+	
+	for (j = 0;  j < 2; j++){
+		for(m = 0; m < MT_NUM; m++){
+			pol[j][m].r = &mds.r[j * MT_NUM*MAX_MT_LENGTH + MAX_MT_LENGTH*m];
+			pol[j][m].n = 2;
+			pol[j][m].state = MT_STATE_POL;
+		}	
 	} 
 
 	KT kt;
@@ -138,65 +137,43 @@ int main(){
 	kt.r = &mds.r[2 * MT_NUM*MAX_MT_LENGTH];
 
 	
-	float3 r0, r1;
-	r0.x = -500; 
-	r0.y = 0; 
-	r0.z = 0;
+	float3 r[2];
+	r[0].x = -3000; 
+	r[0].y = 0; 
+	r[0].z = 0;
 
-	r1.x = 500;
-	r1.y = 0;
-	r1.z = 0;
+	r[1].x = 3000;
+	r[1].y = 0;
+	r[1].z = 0;
 
 
-
-	for(m = 0; m < MT_NUM; m++){
-		float theta = M_PI*ran2(&rseed);
-		float phi = 2.0*M_PI*ran2(&rseed);
-		mt1[m].r[0].x = r0.x + POLE_RADIUS*sin(theta)*cos(phi);
-		mt1[m].r[0].y = r0.y + POLE_RADIUS*sin(theta)*sin(phi);
-		mt1[m].r[0].z = r0.z + POLE_RADIUS*cos(theta);
-		mt1[m].r[1].x = r0.x + (POLE_RADIUS + 2.0*MT_RADIUS)*sin(theta)*cos(phi);
-		mt1[m].r[1].y = r0.y + (POLE_RADIUS + 2.0*MT_RADIUS)*sin(theta)*sin(phi);
-		mt1[m].r[1].z = r0.z + (POLE_RADIUS + 2.0*MT_RADIUS)*cos(theta);
-		float mindist = 100.0;
-		int m1;
-		for(m1 = 0; m1 < m; m1++){
-			float dx = mt1[m1].r[0].x - mt[m].r[0].x;
-			float dy = mt1[m1].r[0].y - mt[m].r[0].y;
-			float dz = mt1[m1].r[0].z - mt[m].r[0].z;
-			float dr = sqrt(dx*dx + dy*dy + dz*dz);
-			if(dr < mindist){
-				mindist = dr;
+	for (j = 0; j < 2; j++){
+		for(m = 0; m < MT_NUM; m++){
+		
+			float theta = M_PI*ran2(&rseed);
+			float phi = 2.0*M_PI*ran2(&rseed);
+			pol[j][m].r[0].x = r[j].x + POLE_RADIUS*sin(theta)*cos(phi);
+			pol[j][m].r[0].y = r[j].y + POLE_RADIUS*sin(theta)*sin(phi);
+			pol[j][m].r[0].z = r[j].z + POLE_RADIUS*cos(theta);
+			pol[j][m].r[1].x = r[j].x + (POLE_RADIUS + 2.0*MT_RADIUS)*sin(theta)*cos(phi);
+			pol[j][m].r[1].y = r[j].y + (POLE_RADIUS + 2.0*MT_RADIUS)*sin(theta)*sin(phi);
+			pol[j][m].r[1].z = r[j].z + (POLE_RADIUS + 2.0*MT_RADIUS)*cos(theta);
+			float mindist = 100.0;
+			int m1;
+			for(m1 = 0; m1 < m; m1++){
+				float dx = pol[j][m1].r[0].x - pol[j][m].r[0].x;
+				float dy = pol[j][m1].r[0].y - pol[j][m].r[0].y;
+				float dz = pol[j][m1].r[0].z - pol[j][m].r[0].z;
+				float dr = sqrt(dx*dx + dy*dy + dz*dz);
+				if(dr < mindist){
+					mindist = dr;
+				}
+			}
+			if(mindist < MT_RADIUS*2.0){
+				m--;
 			}
 		}
-		if(mindist < MT_RADIUS*2.0){
-			m--;
-		}
-	}
-
-	for(m = 0; m < MT_NUM; m++){
-		float theta = M_PI*ran2(&rseed);
-		float phi = 2.0*M_PI*ran2(&rseed);
-		mt2[m].r[0].x = r1.x + POLE_RADIUS*sin(theta)*cos(phi);
-		mt2[m].r[0].y = r1.y + POLE_RADIUS*sin(theta)*sin(phi);
-		mt2[m].r[0].z = r1.z + POLE_RADIUS*cos(theta);
-		mt2[m].r[1].x = r1.x + (POLE_RADIUS + 2.0*MT_RADIUS)*sin(theta)*cos(phi);
-		mt2[m].r[1].y = r1.y + (POLE_RADIUS + 2.0*MT_RADIUS)*sin(theta)*sin(phi);
-		mt2[m].r[1].z = r1.z + (POLE_RADIUS + 2.0*MT_RADIUS)*cos(theta);
-		float mindist = 100.0;
-		int m1;
-		for(m1 = 0; m1 < m; m1++){
-			float dx = mt2[m1].r[0].x - mt[m].r[0].x;
-			float dy = mt2[m1].r[0].y - mt[m].r[0].y;
-			float dz = mt2[m1].r[0].z - mt[m].r[0].z;
-			float dr = sqrt(dx*dx + dy*dy + dz*dz);
-			if(dr < mindist){
-				mindist = dr;
-			}
-		}
-		if(mindist < MT_RADIUS*2.0){
-			m--;
-		}
+		
 	}
 
 	initPSF();
@@ -225,8 +202,8 @@ int main(){
 
 
 		kineticStep();
-		membraneMechanics(mds.r, mds.f);
-		//membraneKinetics();
+		//membraneMechanics(mds.r, mds.f);
+		membraneKinetics();
 		computeHarmonic(mds.r, mds.f);
 		computeAngles(mds.r, mds.f);
 		integrateCPU(mds.r, mds.f, dt, mds.N);
@@ -250,10 +227,11 @@ int main(){
 }
 
 vector<float3> generateKinetochore(){
+	/*
 	float xc = 2 * ran2(&rseed) * ELLIPSE_A - ELLIPSE_A;
 	float yc = 2 * ran2(&rseed) * ELLIPSE_B - ELLIPSE_B;
 	float zc = 2 * ran2(&rseed) * ELLIPSE_C - ELLIPSE_C;
-/*
+
 	float3 dir;
 	dir.x = 2 * ran2(&rseed) - 1.0;
 	dir.y = 2 * ran2(&rseed) - 1.0;
@@ -264,6 +242,10 @@ vector<float3> generateKinetochore(){
 	int n = 0;
 
 */
+	float xc = 0.0;
+	float yc = 0.0;
+	float zc = 0.0;
+
 	float3 center;
 	center.x = xc;
 	center.y = yc;
@@ -326,227 +308,241 @@ int computeGamma(float r){
 }
 
 void membraneKinetics(){
-	int m; 
+	int m,j; 
 	float rx, ry, rz, re2;
-	for(m = 0; m < MT_NUM; m++){
+	for (j = 0; j < 2; j++){
+		for(m = 0; m < MT_NUM; m++){
 
-		if (mt[m].state != MT_STATE_DEPOL){
-			rx = mt[m].r[mt[m].n - 1].x;
-			ry = mt[m].r[mt[m].n - 1].y;
-			rz = mt[m].r[mt[m].n - 1].z;
+			if (pol[j][m].state != MT_STATE_DEPOL){
+				rx = pol[j][m].r[pol[j][m].n - 1].x;
+				ry = pol[j][m].r[pol[j][m].n - 1].y;
+				rz = pol[j][m].r[pol[j][m].n - 1].z;
 
-			re2 = pow(rx, 2) / pow(ELLIPSE_A, 2) + pow(ry, 2) / pow(ELLIPSE_B, 2) + pow(rz, 2) / pow(ELLIPSE_C, 2); 
-			if (re2 > 1.0) {
-				mt[m].state = MT_STATE_DEPOL;
-			}
-		}
-		
-	}
-
-}
-
-void kineticStep(){
-	int m;
-	for(m = 0; m < MT_NUM; m++){
-		float p = ran2(&rseed);
-		if(mt[m].state == MT_STATE_POL){
-			if (p <= MT_CATASTROPHE_FREQ){
-				//printf("catastrophe!\n");
-				mt[m].state = MT_STATE_DEPOL;
-			}
-		} else 
-		if(mt[m].state == MT_STATE_DEPOL){
-			if (p <= MT_RESCUE_FREQ){
-				mt[m].state = MT_STATE_POL;
-			}
-		}
-		if(mt[m].state == MT_STATE_POL && mt[m].n + 1 < MAX_MT_LENGTH){ // Warning needed
-			p = ran2(&rseed);
-			if (p <= MT_POL_RATE){
-				int n = mt[m].n;
-				float dx = mt[m].r[n-1].x - mt[m].r[n-2].x;
-				float dy = mt[m].r[n-1].y - mt[m].r[n-2].y;
-				float dz = mt[m].r[n-1].z - mt[m].r[n-2].z;
-				float dr = sqrt(dx*dx + dy*dy + dz*dz);
-				dx = 2.0*MT_RADIUS*dx/dr;
-				dy = 2.0*MT_RADIUS*dy/dr;
-				dz = 2.0*MT_RADIUS*dz/dr;
-				mt[m].r[n].x = mt[m].r[n-1].x + dx;
-				mt[m].r[n].y = mt[m].r[n-1].y + dy;
-				mt[m].r[n].z = mt[m].r[n-1].z + dz;
-				mt[m].n ++;
-			}
-		}
-		if(mt[m].state == MT_STATE_DEPOL && mt[m].n > 2){ // Warning needed
-			p = ran2(&rseed);
-			if (p <= MT_DEPOL_RATE){
-				//printf("Depol!\n");
-				int n = mt[m].n;
-				mt[m].r[n-1].x = 0.0;
-				mt[m].r[n-1].y = 0.0;
-				mt[m].r[n-1].z = 0.0;
-				mt[m].n --;
-			}
-		}
-	
-	}
-}
-
-void membraneMechanics(float3* r, float3* f){
-	int m, i, mi;
-	float re2, norm;
-
-	for(m = 0; m < MT_NUM; m++){
-		for(mi = 0; mi < mt[m].n-1; mi++){
-
-			i = m * MAX_MT_LENGTH + mi;
-			float3 ri = r[i];
-			re2 = pow(ri.x, 2) / pow(ELLIPSE_A, 2) + pow(ri.y, 2) / pow(ELLIPSE_B, 2) + pow(ri.z, 2) / pow(ELLIPSE_C, 2);
-			norm = sqrt(pow(ri.x, 2) + pow(ri.y, 2) + pow(ri.z, 2)); 
-
-			if (re2 > 1.0){
-				f[i].x -= MEMBRANE_STIFF * (re2 - 1.0) * ri.x / norm;
-				f[i].y -= MEMBRANE_STIFF * (re2 - 1.0) * ri.y / norm;		
-				f[i].z -= MEMBRANE_STIFF * (re2 - 1.0) * ri.z / norm; 
+				re2 = pow(rx, 2) / pow(ELLIPSE_A, 2) + pow(ry, 2) / pow(ELLIPSE_B, 2) + pow(rz, 2) / pow(ELLIPSE_C, 2); 
+				if (re2 > 1.0) {
+					pol[j][m].state = MT_STATE_DEPOL;
+				}
 			}
 			
 		}
 	}
+	
 
+}
+
+void kineticStep(){
+	int m,j;
+	for (j = 0; j < 2; j++){
+		float p = ran2(&rseed);
+		for(m = 0; m < MT_NUM; m++){
+			
+			if(pol[j][m].state == MT_STATE_POL){
+				if (p <= MT_CATASTROPHE_FREQ){
+					//printf("catastrophe!\n");
+					pol[j][m].state = MT_STATE_DEPOL;
+				}
+			} else
+
+			if(pol[j][m].state == MT_STATE_DEPOL){
+				if (p <= MT_RESCUE_FREQ){
+					pol[j][m].state = MT_STATE_POL;
+				}
+			}
+
+			if(pol[j][m].state == MT_STATE_POL && pol[j][m].n + 1 < MAX_MT_LENGTH){ // Warning needed
+				p = ran2(&rseed);
+				if (p <= MT_POL_RATE){
+					int n = pol[j][m].n;
+					float dx = pol[j][m].r[n-1].x - pol[j][m].r[n-2].x;
+					float dy = pol[j][m].r[n-1].y - pol[j][m].r[n-2].y;
+					float dz = pol[j][m].r[n-1].z - pol[j][m].r[n-2].z;
+					float dr = sqrt(dx*dx + dy*dy + dz*dz);
+					dx = 2.0*MT_RADIUS*dx/dr;
+					dy = 2.0*MT_RADIUS*dy/dr;
+					dz = 2.0*MT_RADIUS*dz/dr;
+					pol[j][m].r[n].x = pol[j][m].r[n-1].x + dx;
+					pol[j][m].r[n].y = pol[j][m].r[n-1].y + dy;
+					pol[j][m].r[n].z = pol[j][m].r[n-1].z + dz;
+					pol[j][m].n ++;
+				}
+			}
+			if(pol[j][m].state == MT_STATE_DEPOL && pol[j][m].n > 2){ // Warning needed
+				p = ran2(&rseed);
+				if (p <= MT_DEPOL_RATE){
+					//printf("Depol!\n");
+					int n = pol[j][m].n;
+					pol[j][m].r[n-1].x = 0.0;
+					pol[j][m].r[n-1].y = 0.0;
+					pol[j][m].r[n-1].z = 0.0;
+					pol[j][m].n --;
+				}
+			}
+		
+		}
+	}
+	
+}
+
+void membraneMechanics(float3* r, float3* f){
+	int m, i, mi, j;
+	float re2, norm;
+	for (j = 0; j < 2; j++){
+		for(m = 0; m < MT_NUM; m++){
+			for(mi = 0; mi < pol[j][m].n-1; mi++){
+
+				i = j * MT_NUM * MAX_MT_LENGTH + m * MAX_MT_LENGTH + mi;
+				float3 ri = r[i];
+				re2 = pow(ri.x, 2) / pow(ELLIPSE_A, 2) + pow(ri.y, 2) / pow(ELLIPSE_B, 2) + pow(ri.z, 2) / pow(ELLIPSE_C, 2);
+				norm = sqrt(pow(ri.x, 2) + pow(ri.y, 2) + pow(ri.z, 2)); 
+
+				if (re2 > 1.0){
+					f[i].x -= MEMBRANE_STIFF * (re2 - 1.0) * ri.x / norm;
+					f[i].y -= MEMBRANE_STIFF * (re2 - 1.0) * ri.y / norm;		
+					f[i].z -= MEMBRANE_STIFF * (re2 - 1.0) * ri.z / norm; 
+				}
+				
+			}
+		}
+	}
 }
 
 void computeHarmonic(float3* r, float3* f){
-	int i, j, m, mi;
+	int i, j, m, mi, k;
 
 	float r0 = 2.0*MT_RADIUS;
+	for (k = 0; k < 2; k++){
 
-	for(m = 0; m < MT_NUM; m++){
-		for(mi = 0; mi < mt[m].n-1; mi++){
-			i = m*MAX_MT_LENGTH + mi;
-			j = i + 1;
+		for(m = 0; m < MT_NUM; m++){
+			for(mi = 0; mi < pol[k][m].n-1; mi++){
+				i = k * MT_NUM * MAX_MT_LENGTH + m * MAX_MT_LENGTH + mi;
+				j = i + 1;
 
-			float3 ri = r[i];
-			float3 rj = r[j];
+				float3 ri = r[i];
+				float3 rj = r[j];
 
-			float dx = rj.x - ri.x;
-			float dy = rj.y - ri.y;
-			float dz = rj.z - ri.z;
-	
-			float dr = sqrtf(dx*dx + dy*dy + dz*dz);
-			float df = K_SPRING*(dr - r0)/dr;
+				float dx = rj.x - ri.x;
+				float dy = rj.y - ri.y;
+				float dz = rj.z - ri.z;
 		
-			f[i].x += df*dx;
-			f[i].y += df*dy;		
-			f[i].z += df*dz;
-			f[j].x -= df*dx;
-			f[j].y -= df*dy;
-			f[j].z -= df*dz;
+				float dr = sqrtf(dx*dx + dy*dy + dz*dz);
+				float df = K_SPRING*(dr - r0)/dr;
+			
+				f[i].x += df*dx;
+				f[i].y += df*dy;		
+				f[i].z += df*dz;
+				f[j].x -= df*dx;
+				f[j].y -= df*dy;
+				f[j].z -= df*dz;
+			}
 		}
 	}
+	
 }
 
 void computeAngles(float3* r, float3* f){
-	int i, j, k, m, mi;
+	int i, j, k, m, mi, l;
 
 	float theta0 = M_PI;
+	for (l = 0; l < 2; l++){
+		for(m = 0; m < MT_NUM; m++){
+			for(mi = 0; mi < pol[l][m].n-2; mi++){
+				i = l * MT_NUM * MAX_MT_LENGTH + m * MAX_MT_LENGTH + mi;
+				j = i + 1;
+				k = i + 2;
+			
+				float3 r1 = r[i];
+				float3 r2 = r[j];
+				float3 r3 = r[k];
 
-	for(m = 0; m < MT_NUM; m++){
-		for(mi = 0; mi < mt[m].n-2; mi++){
-			i = m*MAX_MT_LENGTH + mi;
-			j = i + 1;
-			k = i + 2;
-		
-			float3 r1 = r[i];
-			float3 r2 = r[j];
-			float3 r3 = r[k];
+				float3 dr12, dr32;
+				dr12.x = r1.x - r2.x;
+				dr12.y = r1.y - r2.y;
+				dr12.z = r1.z - r2.z;
 
-			float3 dr12, dr32;
-			dr12.x = r1.x - r2.x;
-			dr12.y = r1.y - r2.y;
-			dr12.z = r1.z - r2.z;
+				dr32.x = r3.x - r2.x;
+				dr32.y = r3.y - r2.y;
+				dr32.z = r3.z - r2.z;
 
-			dr32.x = r3.x - r2.x;
-			dr32.y = r3.y - r2.y;
-			dr32.z = r3.z - r2.z;
-
-			float r12inv = 1.0f/sqrtf(dr12.x*dr12.x + dr12.y*dr12.y + dr12.z*dr12.z);
-			float r32inv = 1.0f/sqrtf(dr32.x*dr32.x + dr32.y*dr32.y + dr32.z*dr32.z);
-			float costheta = (dr12.x*dr32.x + dr12.y*dr32.y + dr12.z*dr32.z)*r12inv*r32inv;
-			if(costheta > 1.0f){
-				costheta = 1.0f;
-			} else
-			if(costheta < -1.0f){
-				costheta = -1.0f;
-			}
-			float sintheta = sqrtf(1.0f - costheta*costheta);
-			float theta = acos(costheta);
-			float diff = theta - theta0;
-			if(sintheta < 1.e-6){
-				if(diff < 0){
-					diff *= 2.0f*K_THETA;
-				} else {
-					diff *= -2.0f*K_THETA;
+				float r12inv = 1.0f/sqrtf(dr12.x*dr12.x + dr12.y*dr12.y + dr12.z*dr12.z);
+				float r32inv = 1.0f/sqrtf(dr32.x*dr32.x + dr32.y*dr32.y + dr32.z*dr32.z);
+				float costheta = (dr12.x*dr32.x + dr12.y*dr32.y + dr12.z*dr32.z)*r12inv*r32inv;
+				if(costheta > 1.0f){
+					costheta = 1.0f;
+				} else
+				if(costheta < -1.0f){
+					costheta = -1.0f;
 				}
-			} else {
-				diff *= (-2.0f*K_THETA) / sintheta;
+				float sintheta = sqrtf(1.0f - costheta*costheta);
+				float theta = acos(costheta);
+				float diff = theta - theta0;
+				if(sintheta < 1.e-6){
+					if(diff < 0){
+						diff *= 2.0f*K_THETA;
+					} else {
+						diff *= -2.0f*K_THETA;
+					}
+				} else {
+					diff *= (-2.0f*K_THETA) / sintheta;
+				}
+				float c1 = diff*r12inv;
+				float c2 = diff*r32inv;
+
+				float3 f1, f2, f3;
+				f1.x = c1*(dr12.x*(r12inv*costheta) - dr32.x*r32inv);
+				f1.y = c1*(dr12.y*(r12inv*costheta) - dr32.y*r32inv);
+				f1.z = c1*(dr12.z*(r12inv*costheta) - dr32.z*r32inv);
+				f2 = f1;
+				f3.x = c2*(dr32.x*(r32inv*costheta) - dr12.x*r12inv);
+				f3.y = c2*(dr32.y*(r32inv*costheta) - dr12.y*r12inv);
+				f3.z = c2*(dr32.z*(r32inv*costheta) - dr12.z*r12inv);
+				f2.x += f3.x;
+				f2.y += f3.y;
+				f2.z += f3.z;
+
+				f2.x = -f2.x;
+				f2.y = -f2.y;
+				f2.z = -f2.z;
+
+				f[i].x += f1.x;
+				f[i].y += f1.y;
+				f[i].z += f1.z;
+
+				f[j].x += f2.x;
+				f[j].y += f2.y;
+				f[j].z += f2.z;
+
+				f[k].x += f3.x;
+				f[k].y += f3.y;
+				f[k].z += f3.z;
 			}
-			float c1 = diff*r12inv;
-			float c2 = diff*r32inv;
-
-			float3 f1, f2, f3;
-			f1.x = c1*(dr12.x*(r12inv*costheta) - dr32.x*r32inv);
-			f1.y = c1*(dr12.y*(r12inv*costheta) - dr32.y*r32inv);
-			f1.z = c1*(dr12.z*(r12inv*costheta) - dr32.z*r32inv);
-			f2 = f1;
-			f3.x = c2*(dr32.x*(r32inv*costheta) - dr12.x*r12inv);
-			f3.y = c2*(dr32.y*(r32inv*costheta) - dr12.y*r12inv);
-			f3.z = c2*(dr32.z*(r32inv*costheta) - dr12.z*r12inv);
-			f2.x += f3.x;
-			f2.y += f3.y;
-			f2.z += f3.z;
-
-			f2.x = -f2.x;
-			f2.y = -f2.y;
-			f2.z = -f2.z;
-
-			f[i].x += f1.x;
-			f[i].y += f1.y;
-			f[i].z += f1.z;
-
-			f[j].x += f2.x;
-			f[j].y += f2.y;
-			f[j].z += f2.z;
-
-			f[k].x += f3.x;
-			f[k].y += f3.y;
-			f[k].z += f3.z;
 		}
 	}
-
-
+	
 }
 
 void integrateCPU(float3* r, float3* f, float dt, int N){
 
-	int i, m, mi;
+	int i, m, mi, j;
 
 	float gamma = computeGamma(MT_RADIUS);
 
 	double var = sqrtf(KB*T*2.0*dt/gamma);
+	for (j = 0; j < 2; j++){
+		for(m = 0; m < MT_NUM; m++){
+			for(mi = 1; mi < pol[j][m].n; mi++){
+				i = j * MT_NUM * MAX_MT_LENGTH + m*MAX_MT_LENGTH + mi;
+		
+				r[i].x += f[i].x*dt/gamma + var*gasdev(&rseed);
+				r[i].y += f[i].y*dt/gamma + var*gasdev(&rseed);
+				r[i].z += f[i].z*dt/gamma + var*gasdev(&rseed);	
 
-	for(m = 0; m < MT_NUM; m++){
-		for(mi = 1; mi < mt[m].n; mi++){
-			i = m*MAX_MT_LENGTH + mi;
-	
-			r[i].x += f[i].x*dt/gamma + var*gasdev(&rseed);
-			r[i].y += f[i].y*dt/gamma + var*gasdev(&rseed);
-			r[i].z += f[i].z*dt/gamma + var*gasdev(&rseed);	
-
-			f[i].x = 0.0f;
-			f[i].y = 0.0f;
-			f[i].z = 0.0f;
+				f[i].x = 0.0f;
+				f[i].y = 0.0f;
+				f[i].z = 0.0f;
+			}
 		}
 	}
+	
 
 }
 
@@ -574,7 +570,7 @@ int initPSF(){
 	psf.nimphi = 0;
 	psf.ncmap = 0;
 	psf.nnb = 0;
-	int i, j, m, mi, b;
+	int i, j, m, mi, b, l;
 	for(i = 0; i < psf.natom; i++){
 		psf.atoms[i].id = i + 1;
 		if(i < mds.N){
@@ -591,14 +587,19 @@ int initPSF(){
 	float r0 = 2.0*MT_RADIUS;
 
 	b = 0;
-	for(m = 0; m < MT_NUM; m++){
-		for(mi = 0; mi < MAX_MT_LENGTH-1; mi++){
-			psf.bonds[b].i = m*MAX_MT_LENGTH + mi + 1;
-			psf.bonds[b].j = m*MAX_MT_LENGTH + mi + 2;
-			b++;
-		}
-	}
+	/*
+	for (l = 0; l < 2 ; l++){
+		for(m = 0; m < MT_NUM; m++){
+			for(mi = 0; mi < MAX_MT_LENGTH-1; mi++){
 
+				psf.bonds[b].i = l * MT_NUM * MAX_MT_LENGTH + m*MAX_MT_LENGTH + mi + 1;
+				psf.bonds[b].j = l * MT_NUM * MAX_MT_LENGTH + m*MAX_MT_LENGTH + mi + 2;
+				b++;
+			}
+		}
+
+	}
+	*/
 	char filename[1024];
 
 	sprintf(filename, "output/MT.psf");
